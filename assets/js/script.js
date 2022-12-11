3f596e92c6356c7438880cc8a62880f1880d8296
Cole Roberts <coleroberts76@gmail.com>
2022-12-11 16:07:24 -0600

    added all assignment code as well as variable declaration code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate"); // <button id="generate" class="btn">Generate Password</button>
+// Assignment Code to prompt user with questions when generate button is clicked
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-console.log("generate");
+var number = ["0", "1", "2", "4", "5", "6", "8", "9"];
+var specialChar = ["!", "%", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// 72 characters able to be generated with
+console.log("generate", number, specialChar, alphaLower, alphaUpper);
+
+// var declaration
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
