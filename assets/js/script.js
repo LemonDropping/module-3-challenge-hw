b9193e3fb1d65d2f556300c5146cc83eadcc9c0f
Cole Roberts <coleroberts76@gmail.com>
2022-12-11 18:40:05 -0600

    completed javascript coding and generate button works, need to fix the prompt windows to allow user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,61 @@ var confirmNumericCharacter;
 var confirmUpperCase;
 var confirmLowerCase;
 
-// Write password to the #password input
+// Prompt user to confirm how many characters they want in their password, minimum of 8
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to have?"));
+
+
+//if answer is outside the parameters
+while(confirmLength <= 7 || confirmLength >= 51) {
+  alert("Password length must be between 8-32 characters, please try again");
+  var confirmLength = (prompt("How many characters would you like your password to have?"));
+  }
+
+// Message for how many characters the user will have
+alert(`Your password will have ${confirmLength} characters`);
+
+// Password rules 
+var confirmLowerCase = confirm ("Click OK to add lowercase characters");
+var confirmUpperCase = confirm ("Click OK to add uppercase characters");
+var confirmSpecialCharacter = confirm ("Click OK to add special characters");
+var confirmNumericCharacter = confirm ("Click OK to add numeric characters");
+
+// If answer does not follow rules
+while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+  alert("You must choose at least one of each parameter")
+}
+
+// Assign actions to the password rules 
+var passwordCharacters = []
+  if (confirmSpecialCharacter) {
+    passwordCharacters = passwordCharacters.concat(specialChar)
+  }
+  if (confirmNumericCharacter) {
+    passwordCharacters = passwordCharacters.concat(number)
+  }
+  if (confirmLowerCase) {
+    passwordCharacters = passwordCharacters.concat(alphaLower)
+  }
+  if (confirmUpperCase) {
+    passwordCharacters = passwordCharacters.concat(alphaUpper)
+  }
+
+  console.log(passwordCharacters)
+
+// Array for random characters looping
+var randomPassword = ""
+for (var i = 0; i < confirmLength; i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  console.log(randomPassword)
+}
+return randomPassword;
+}
+
+// for password input 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-  
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
